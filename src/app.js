db04ccc846a35b8d15dba1796c3a64d93bc42ce7
Juanjo <juanjosearceherrera@gmail.com>
2024-05-18 15:41:12 -0300

    Primer hola mundo desde back

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-const momento = require('moment');
-console.log(momento());
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+res.end("Mi primer hola mundo desde backend")
+})
+
+server.listen(8080,()=>{
+console.log("Listering on port 8080")
+})
