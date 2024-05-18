403cb6302e2dc65360c4be45de40362a986e14b7
Juanjo <juanjosearceherrera@gmail.com>
2024-05-18 20:37:13 -0300

    implementancion de endpoint que utiliza req.query

--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ import express from 'express';
 const app = express();
 
 app.use(express.urlencoded({extended:true}))
-app.get('/ejemploQueries',(req,res)=>{
-let consultas = req.query;
-let {nombre,apellido,edad} = req.query;
-res.send(consultas)
-})
-app.listen(8080,()=>console.log("Listo para probar queries"))
+const usuarios = [
+    {id:"1",nombre:"Dalia",apellido:"Gomez",genero:"F"},
+    {id:"2",nombre:"Myrna",apellido:"Flores",genero:"F"},
+    {id:"3",nombre:"Armando",apellido:"Mendoza",genero:"M"},
+    {id:"4",nombre:"Dalia",apellido:"Gomez",genero:"F"},
+    {id:"5",nombre:"Herminio",apellido:"Fuentes",genero:"M"},
+    {id:"6",nombre:"Juan",apellido:"Zepeda",genero:"M"},
+    ]
+    
+    app.get('/',(req,res)=>{
+    let genero = req.query.genero;
+    //Si no se ingreso o el genero no es M ni es F no vale el filtro
+    if(!genero||(genero!=="M"&&genero!=="F")) return res.send({usuarios})
+    //en caso contrario continuamos con el proceso de filtro
+    let usuariosFiltrados = usuarios.filter(usuario=>usuario.genero===genero);
+    res.send({usuarios:usuariosFiltrados})
+    })
+    
+    app.listen(8080,()=>console.log("Preparado para hacer filtros"))
